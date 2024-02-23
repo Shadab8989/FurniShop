6ef937ca3516263d00e40238b1a3ab6a814cac83
Shadab8989 <107117111+Shadab8989@users.noreply.github.com>
2024-02-23 10:31:53 +0530

    Updated

--- api/index.js
+++ api/index.js
@@ -8,7 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+	cors({
+		origin: ["https://furni-shop-api.vercel.app"],
+		methods: ["POST", "GET", "PATCH", "PUT", "DELETE"],
+		credentials: true,
+	})
+);
 
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
