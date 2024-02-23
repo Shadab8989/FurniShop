eef8d6d73e41850c4d2980951aa8dd048256cafd
Shadab8989 <107117111+Shadab8989@users.noreply.github.com>
2024-02-23 09:32:31 +0530

    updated

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-	return mongoose.connect(url);
+	return mongoose.connect(url,{
+
+		useNewUrlParser: "true",
+		useUnifiedTopology: "true"
+	  
+	  });
 };
 
 module.exports = connectDB;
